804248e80efb9db3723db122933b3c17ef3f3c7e
TuanTrungVU <trung.lespace@gmail.com>
2019-09-09 10:50:22 +0200

    Init project

---
+++ UserDirStructure/UserDirStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserDirStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
